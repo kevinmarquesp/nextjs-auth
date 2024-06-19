ccc994ab519982dd2f5d3d1816b0b2739b237136
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-19 16:12:49 -0300

    :recycle: refactoring: The home page has a similar spacing strategy to the login page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function App() {
   // const current_session = null
 
   return (
-    <main className="w-screen h-screen p-4 bg-black">
+    <main className="w-screen h-screen p-8 bg-black">
       <div className="max-w-sm">
-        <pre className="w-full px-8 py-4 mb-3 bg-zinc-900 rounded-md text-green-500">
+        <pre className="w-full px-8 py-4 mb-6 bg-zinc-900 rounded-md text-green-500">
           {JSON.stringify(current_session, null, 2)}
         </pre>
         <a href="/login" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md">
