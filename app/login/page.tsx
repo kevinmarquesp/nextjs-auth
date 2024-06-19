05ce30bffba914579579f74905edbe97f3cc02a4
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-19 16:29:30 -0300

    :recycle: refactoring: Now the login page source code was abstracted into different react components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,39 +2,41 @@ export default function Login() {
   return (
     <main className="w-screen h-screen p-8 bg-black">
       <form action="" className="max-w-sm">
+
         <div className="mb-3">
-          <input
-            className="w-full px-8 py-4 mb-3 bg-zinc-900 rounded-md text-white border border-zinc-900 focus:outline-none focus:border-green-800"
-            required
-            type="email"
-            placeholder="Email"
-            id="email"
-            name="email"
-          />
-          <input
-            className="w-full px-8 py-4 mb-3 bg-zinc-900 rounded-md text-white border border-zinc-900 focus:outline-none focus:border-green-800"
-            required
-            type="text"
-            placeholder="Username"
-            id="username"
-            name="username"
-          />
-          <input
-            className="w-full px-8 py-4 mb-3 bg-zinc-900 rounded-md text-white border border-zinc-900 focus:outline-none focus:border-green-800"
-            required
-            type="password"
-            placeholder="Password"
-            id="password"
-            name="password"
-          />
+          <FormInput required type="email" placeholder="Email" name="email" />
+          <FormInput required type="text" placeholder="Username" name="username" />
+          <FormInput required type="password" placeholder="Password" name="password" />
         </div>
+
         <button type="submit" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-zinc-700 hover:bg-zinc-800 transition-all ease-linear font-bold rounded-md">
           Enter with Google
         </button>
+
         <button type="submit" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md">
           Register
         </button>
+
       </form>
     </main >
   );
+}
+
+function FormInput(props: Readonly<{
+  className?: string,
+  required?: boolean,
+  type: string,
+  placeholder: string,
+  name: string,
+}>) {
+  return (
+    <input
+      className={'w-full px-8 py-4 mb-3 bg-zinc-900 rounded-md text-white border border-zinc-900 focus:outline-none focus:border-green-800 ' + (props.className ? props.className : '')}
+      required={props.required ? true : false}
+      type={props.type}
+      placeholder={props.placeholder}
+      id={props.name}
+      name={props.name}
+    />
+  )
 }
