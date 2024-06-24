46f316d74129f69b6450313ba9bc19d013cf723d
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-24 14:59:46 -0300

    :truck: mv: The login page now is nammed as signin page properlly, typo of mine, sorry

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function App() {
-  const current_session = { msg: 'Hello', data: { foo: 'foo', bar: 'bar', baz: 'baz' } }
-  // const current_session = null
+  // const current_session = { msg: 'Hello', data: { foo: 'foo', bar: 'bar', baz: 'baz' } }
+  const current_session = null
 
   return (
     <main className="w-screen h-screen p-8 bg-black">
@@ -8,14 +8,17 @@ export default function App() {
         <pre className="w-full px-8 py-4 mb-6 bg-zinc-900 rounded-md text-green-500">
           {JSON.stringify(current_session, null, 2)}
         </pre>
-        <a href="/login" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md">
+        <a href="/login" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-zinc-700 hover:bg-zinc-800 transition-all ease-linear font-bold rounded-md">
           Login
         </a>
+
         {current_session ?
           <button className="inline-block w-full px-8 py-4 text-center text-white bg-red-700 hover:bg-red-800 transition-all ease-linear font-bold rounded-md">
             Logout
           </button>
-          : <></>}
+          : <a href="/signin" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md">
+            Signin
+          </a>}
       </div>
     </main >
   );
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@
 
 import { signIn } from "next-auth/react";
 
-export default function Login() {
+export default function SignIn() {
   return (
     <main className='w-screen h-screen p-8 bg-black'>
       <form action={async (data) => {
@@ -13,15 +13,11 @@ export default function Login() {
         })
       }} className='max-w-sm'>
 
-        <div className='mb-3'>
+        <div>
           <FormInput required type='email' placeholder='Email' name='email' />
           <FormInput required type='text' placeholder='Username' name='username' />
           <FormInput required type='password' placeholder='Password' name='password' />
         </div>
-
-        <button type='submit' className='inline-block w-full px-8 py-4 mb-3 text-center text-white bg-zinc-700 hover:bg-zinc-800 transition-all ease-linear font-bold rounded-md'>
-          Enter with Google
-        </button>
 
         <button type='submit' className='inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md'>
           Register
