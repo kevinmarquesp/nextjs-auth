2cc934fc579547166679e777f54078d8d17f0a69
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-24 13:48:13 -0300

    :construction: wip: First atempt to add a login system with Google auth provider

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,29 @@
+'use client'
+
+import { signIn } from "next-auth/react";
+
 export default function Login() {
   return (
-    <main className="w-screen h-screen p-8 bg-black">
-      <form action="" className="max-w-sm">
+    <main className='w-screen h-screen p-8 bg-black'>
+      <form action={async (data) => {
+        const signinResponse = await signIn("credentials", {
+          email: data.get("email"),
+          username: data.get("username"),
+          password: data.get("password"),
+        })
+      }} className='max-w-sm'>
 
-        <div className="mb-3">
-          <FormInput required type="email" placeholder="Email" name="email" />
-          <FormInput required type="text" placeholder="Username" name="username" />
-          <FormInput required type="password" placeholder="Password" name="password" />
+        <div className='mb-3'>
+          <FormInput required type='email' placeholder='Email' name='email' />
+          <FormInput required type='text' placeholder='Username' name='username' />
+          <FormInput required type='password' placeholder='Password' name='password' />
         </div>
 
-        <button type="submit" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-zinc-700 hover:bg-zinc-800 transition-all ease-linear font-bold rounded-md">
+        <button type='submit' className='inline-block w-full px-8 py-4 mb-3 text-center text-white bg-zinc-700 hover:bg-zinc-800 transition-all ease-linear font-bold rounded-md'>
           Enter with Google
         </button>
 
-        <button type="submit" className="inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md">
+        <button type='submit' className='inline-block w-full px-8 py-4 mb-3 text-center text-white bg-green-700 hover:bg-green-800 transition-all ease-linear font-bold rounded-md'>
           Register
         </button>
 
---
+++ lib/auth.ts
@@ -0,0 +1,49 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { NextAuthOptions } from 'next-auth';
+import prisma from '@/prisma/client';
+
+export const authConfig: NextAuthOptions = {
+	providers: [
+		CredentialsProvider({
+			name: "Sign in with...",
+
+			credentials: {
+				email: { label: "Email", type: "email", placeholder: "fulanodetal@email.com" },
+				username: { label: "Username", type: "text", placeholder: "Fulano" },
+				password: { label: "Password", type: "password", placeholder: "Password" },
+			},
+
+			async authorize(credentials, _request) {
+				if (!credentials || !credentials.password || !credentials.email || !credentials.password)
+					return null
+
+				let dbUser: any = prisma.user.findUnique({
+					where: { email: credentials.email },
+				})
+
+				if (!dbUser)
+					dbUser = await prisma.user.create({
+						data: {
+							email: credentials.email,
+							username: credentials.username,
+							password: credentials.password,
+						}
+					})
+				else
+					return null
+
+				const user = {
+					id: dbUser.id,
+					email: dbUser.email,
+					username: dbUser.username,
+					createdAt: dbUser.createdAt,
+					updatedAt: dbUser.updatedAt,
+				};
+
+				console.log(user)
+
+				return user
+			},
+		}),
+	],
+}
